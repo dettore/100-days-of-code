3e981accec40a4a3f79ed7fc337de436989a5d6f
Don Ettore <don.ettore@gmail.com>
2023-12-19 09:15:25 -0500

    simple variables with formatted print

---
+++ day-002-simple-variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Create variables for your name, age, and favorite color and print them.
+
+if __name__ == "__main__":
+    name = 'Don'
+    age = 58
+    fav_color = 'blue'
+
+    print(f'My name is {name}, I am {age}, and my favorite color is '
+          f'{fav_color}.')
