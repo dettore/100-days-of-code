f36ecbcbe36d08ffd10023d42540c24ad825c81c
Don Ettore <don.ettore@gmail.com>
2023-12-19 09:32:20 -0500

    simple arithmatic

---
+++ day-003-simple-arithmatic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Perform basic arithmetic operations (add, subtract, multiply, divide)
+
+if __name__ == "__main__":
+    var_a = 2
+    var_b = 4
+
+    print('add: ' + str(var_a + var_b))
+    print('subtract: ' + str(var_b - var_a))
+    print('multiply: ' + str(var_a * var_b))
+    print('divide: ' + str(var_b / var_a))
