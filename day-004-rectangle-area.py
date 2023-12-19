2eda4e98aaa330039263028518a82485cb3fad5c
Don Ettore <don.ettore@gmail.com>
2023-12-19 14:13:17 -0500

    area of a rectangle

---
+++ day-004-rectangle-area.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Calculate the area of a rectangle
+# A = W x L
+
+if __name__ == "__main__":
+    width = 2
+    length = 4
+
+    print('Area: ' + str(width * length) + ' square feet.')
