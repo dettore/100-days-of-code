bdfb44f29124d96de6b083d53112849c9bc4a9ef
Don Ettore <don.ettore@gmail.com>
2023-12-20 15:41:47 -0500

    generate a random num

---
+++ day-008-gen-rand-num.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import random
+
+# Generate a random number
+
+if __name__ == "__main__":
+    random_num = random.randint(1, 100)
+    print("random number between 1 and 100: " + str(random_num))
