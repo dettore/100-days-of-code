d4fe065d30bf755445a64b9e53d6a4e5905da39d
Don Ettore <don.ettore@gmail.com>
2023-12-21 13:51:11 -0500

    while loop to print even num

---
+++ day-010-even-while-loop.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Use a while loop to print even numbers from 2 to 20.
+
+if __name__ == "__main__":
+    n = 1
+    while n <= 20:
+        if (n % 2) == 0:
+            print(n)
+        n += 1
