d79118f270aa6cf41bcbde99b5e2b50040ee10f4
Don Ettore <don.ettore@gmail.com>
2023-12-22 14:45:15 -0500

    print factorial of a num

---
+++ day-014-calc-factorial.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import math
+
+# Write a function to calculate the factorial of a number
+
+n = 4
+
+if __name__ == "__main__":
+    # there is a built-in function to do this
+    factorial = math.factorial(n)
+    print(f"The factorial of {n} is {factorial}")
