1674c0c1eb63e33daa482e5f32ede58f3f24d9d0
Don Ettore <don.ettore@gmail.com>
2023-12-22 12:17:49 -0500

    show if odd or even

---
+++ day-011-even-or-odd.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Check if a number is even or odd
+
+if __name__ == "__main__":
+    n = 16
+    if (n % 2):
+        print(f"The number {n} is odd")
+    else:
+        print(f"The number {n} is even")
